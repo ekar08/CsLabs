19019c7a00a9427a87d97f1390269d82d8434b99
geyue <ekarkar2208@gmail.com>
2026-09-25 23:46:57 +0500

    Solo_mode check for count players

--- sem-1-lab-02-server-config/Program.cs
+++ sem-1-lab-02-server-config/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.Metadata;
+﻿using System.Linq.Expressions;
+using System.Reflection.Metadata;
 
 namespace sem_1_lab_02_server_config;
 
@@ -21,11 +22,29 @@ class Program
         string tmp_count_pl = Console.ReadLine();
         int count_pl = Convert.ToInt32(tmp_count_pl);
 
-        /*
+        string result = "";
+        
         if (tmp_game_mode == "1")
         {
-            
-        } */
+            result = Solo_mode(count_pl);
+        } 
+        Console.WriteLine(result);
+    }
+
+    public static string Solo_mode(int buff_count_pl)
+    {
+        string result_solo="";
+        int max_pl = 50;
+        int min_pl_solo = 3;
+
+        if (buff_count_pl<min_pl_solo)
+            result_solo = "Сервер запустить невозможно, слишком мало игроков, подождите еще";
+        else if (buff_count_pl>max_pl)
+            result_solo = "Слишком много игроков, часть из них уйдет в ожидание след раунда\nВведите код админа";
+        else
+            result_solo = "Сервер можно запускать";
+
+        return result_solo;
     }
 
 }
